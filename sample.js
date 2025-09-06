6ba0158dc6f7c0e9dc1c20701d6b26bdc4437378
siva jyothi <pailasivajyothi46gmail.com>
2025-09-06 12:33:49 +0530

    learned data types in js

--- sample.js
+++ sample.js
@@ -1,1 +1,23 @@
 console.log("this is a sample file")
+let name ="dev astra"
+console.log(name)
+let num=18
+let islogged=false
+let x
+let y=null
+console.log(num)
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof islogged)
+console.log(typeof x)
+console.log(typeof y)
+let arr=[1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+let person={
+    name:"jyothi",
+    age:18
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
