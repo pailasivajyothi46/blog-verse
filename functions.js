ed672b99b4f73b09fe8b67f96982ef61f93c0f31
siva jyothi <pailasivajyothi46gmail.com>
2025-09-06 15:56:30 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,23 @@
+function greet(){
+    console.log("welcome to dev astra")
+}
+greet()
+function add(a,b){
+    return a+b
+}
+let result=add(5,4)
+console.log(result)
+const pi= 3.14
+console.log(pi)
+const  multifly=function(x,y){
+    return x*y
+}
+console.log(multifly(2,4))
+//array function
+const square=(x)=>x*x;
+console.log(square(3))
+const subtract=(x,y)=>{
+    console.log(x,y)
+    return x-y
+}
+console.log(subtract(5,1))
